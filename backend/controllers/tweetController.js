5b61df35fc27f3e6808da3a28a805c15a04bf95d
Minit <minitjain.work@gmail.com>
2025-08-01 21:01:59 +0530

    Add functionality: Added tweet like or dislike logic in backend with successful testing

--- backend/controllers/tweetController.js
+++ backend/controllers/tweetController.js
@@ -1,3 +1,4 @@
+import express from "express";
 import { Tweet } from "../models/tweetSchema.js";
 
 export const createTweet = async (req, res) => {
@@ -39,5 +40,46 @@ export const deleteTweet = async (req, res) => {
     });
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const likeorDislikeTweet = async (req, res) => {
+  try {
+    const LoggedInUserId = req.body.id;
+    const tweetId = req.params.id;
+
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+      return res.status(404).json({
+        message: "Tweet not found.",
+        success: false,
+      });
+    }
+
+    if (tweet.like.includes(LoggedInUserId)) {
+      // If the user has already liked the tweet, so we remove the like
+      await Tweet.findByIdAndUpdate(tweetId, {
+        $pull: { like: LoggedInUserId },
+      });
+
+      return res.status(200).json({
+        message: "User disliked your tweet.",
+        success: true,
+      });
+    } else {
+      await Tweet.findByIdAndUpdate(tweetId, {
+        $push: { like: LoggedInUserId },
+      });
+      return res.status(200).json({
+        message: "User liked your tweet.",
+        success: true,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Something went wrong while liking or disliking the tweet.",
+      success: false,
+    });
   }
 };
--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { createTweet, deleteTweet } from "../controllers/tweetController.js";
+import {
+  createTweet,
+  deleteTweet,
+  likeorDislikeTweet,
+} from "../controllers/tweetController.js";
 import isAuthenticated from "../config/auth.js";
 
 const router = express.Router();
 
 router.post("/create", isAuthenticated, createTweet);
 router.delete("/delete/:id", isAuthenticated, deleteTweet);
-
+router.put("/like/:id", isAuthenticated, likeorDislikeTweet);
 export default router;
