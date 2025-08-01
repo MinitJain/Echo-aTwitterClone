066ae8df1d93adf5b08582fb4eca5e17e62d263b
Minit <minitjain.work@gmail.com>
2025-08-01 19:08:48 +0530

    bug fix: delete tweet functionality fix

--- backend/controllers/tweetController.js
+++ backend/controllers/tweetController.js
@@ -32,7 +32,7 @@ export const createTweet = async (req, res) => {
 export const deleteTweet = async (req, res) => {
   try {
     const { id } = req.params;
-    const tweet = await Tweet.findByIdAndDelete(id);
+    await Tweet.findByIdAndDelete(id);
     return res.status(200).json({
       message: "Tweet deleted successfully.",
       success: true,
--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createTweet } from "../controllers/tweetController.js";
+import { createTweet, deleteTweet } from "../controllers/tweetController.js";
 import isAuthenticated from "../config/auth.js";
 
 const router = express.Router();
+
 router.post("/create", isAuthenticated, createTweet);
-router.delete("/delete/:id", deleteTweet);
+router.delete("/delete/:id", isAuthenticated, deleteTweet);
 
 export default router;
