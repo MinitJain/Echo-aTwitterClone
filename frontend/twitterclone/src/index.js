c01e876097e65836557bfbb8feda7ca502fc2fa2
Minit <minitjain.work@gmail.com>
2025-08-08 23:14:14 +0530

    Implemented react redux toolkit, created, userSlice, Store files. Wrapped  <App/> component in the index.js with <Provider>

--- frontend/twitterclone/src/index.js
+++ frontend/twitterclone/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./index.css";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode store={store}>
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
