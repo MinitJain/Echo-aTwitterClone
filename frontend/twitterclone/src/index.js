a48efd9d034fe41239bdb276176eef2754c2204a
Minit <minitjain.work@gmail.com>
2026-01-28 17:59:11 +0530

    Revert "feat: add @vercel/analytics for enhanced tracking and analytics"
    This reverts commit c4bb625831a5a13cdde1975c3eed8b2a005fc0ff.

--- frontend/twitterclone/src/index.js
+++ frontend/twitterclone/src/index.js
@@ -1,4 +1,3 @@
-// frontend/twitterclone/src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -7,7 +6,6 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { CopilotKit } from "@copilotkit/react-core";
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,14 +13,15 @@ root.render(
     <CopilotKit
       publicApiKey="ck_pub_00828bb5867d63cbfb2bbc6f1352e642"
       chatApiEndpoint="/api/chat"
-      apiConfiguration={{ debug: true }}
+      apiConfiguration={{
+        debug: true,
+      }}
     >
       <Provider store={store}>
         <BrowserRouter>
           <App />
-          <Analytics />
         </BrowserRouter>
       </Provider>
     </CopilotKit>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
